0a55f3b290400d516241f1b6ac185e06ab5354d9
markzyu <7296488+markzyu@users.noreply.github.com>
2021-06-17 14:47:42 -0700

    Multithreaded (ptrace on main thread, prints & parsing on another)

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,6 +1,5 @@
 use nix::sys;
 use std::thread;
-use std::time::Duration;
 
 use std::collections::HashMap;
 
@@ -13,26 +12,38 @@ fn syscall_names() -> HashMap<ptrace::SysNum, String> {
     map
 }
 
-fn main() {
+pub fn event_thread(pid: nix::unistd::Pid, ptrace_client: executor::PtraceClient) {
     let names = syscall_names();
-
-    let mut cmd = std::process::Command::new("ls");
-    let child = ptrace::start(&mut cmd).unwrap();
-    let pid = ptrace::pid(&child).unwrap();
-    dbg!(pid);
     loop {
-        sys::ptrace::syscall(pid, None).unwrap();
-        let status = ptrace::wait_hang(&child).unwrap();
+        ptrace_client.execute(move || sys::ptrace::syscall(pid, None).unwrap());
+        let status = ptrace::waitpid_hang(pid).unwrap();
         dbg!(status);
 
         if !ptrace::is_trace_stop(&status) && !ptrace::is_still_alive(&status) {
             break;
         }
-        let regs = ptrace::getregs(pid).unwrap();
+        let regs = ptrace_client.execute(move || ptrace::getregs(pid).unwrap());
         let unknown: String = "Unknown syscall".into();
         let name = names.get(&regs.syscall_num).unwrap_or(&unknown);
         dbg!(name);
     }
-    thread::sleep(Duration::from_millis(10));
+}
+
+fn main() {
+    let (ptrace_clients, ptrace_loop) = executor::new_ptrace_executor();
+
+    let mut cmd = std::process::Command::new("ls");
+    let child = ptrace::start(&mut cmd).unwrap();
+
+    let pid1 = ptrace::pid(&child).unwrap();
+    dbg!(pid1);
+    let proc1_client = ptrace_clients.clone();
+    thread::spawn(move || {
+        let proc1_client2 = proc1_client.clone();
+        event_thread(pid1, proc1_client);
+        proc1_client2.stop();
+    });
+
+    ptrace_loop.serve();
     dbg!("Done");
 }
