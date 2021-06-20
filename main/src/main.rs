eee2def17a18d52141c7b15ec0898e7a09e4b609
markzyu <7296488+markzyu@users.noreply.github.com>
2021-06-20 12:32:05 -0700

    Modularize event_thread() => sysaug::TraceeHandler

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,23 +1,6 @@
-use lazy_static::lazy_static;
-use nix::sys;
-use ptrace::GenericPurposeRegs;
-use std::collections::HashMap;
-use std::convert::TryInto;
 use std::thread;
 use thiserror::Error;
-use tracing::{event, span, Level};
-
-lazy_static! {
-    static ref SYSCALL_NAMES: HashMap<ptrace::SysNum, String> = {
-        let mut map = HashMap::new();
-        map.insert(libc::SYS_openat, "openat".into());
-        map.insert(libc::SYS_close, "close".into());
-        map.insert(libc::SYS_read, "read".into());
-        map.insert(libc::SYS_write, "write".into());
-        map.insert(libc::SYS_clone, "clone".into());
-        map
-    };
-}
+use tracing::{event, Level};
 
 #[derive(Debug, Error)]
 pub enum CLIError {
@@ -27,143 +10,8 @@ pub enum CLIError {
     #[error("Ptrace error: {0}")]
     Ptrace(#[from] ptrace::PtraceError),
 
-    #[error("OS Error: {0}")]
-    LinuxOSErr(#[from] nix::Error),
-
-    #[error("Interger conversion error")]
-    IntoInt,
-}
-
-trait AugmentSyscall {
-    fn before_call(&self, regs: &GenericPurposeRegs) -> Result<(), CLIError>;
-    fn after_call(&self, regs: &GenericPurposeRegs) -> Result<(), CLIError>;
-    fn dispatch(&self, last_syscall_times: u64, regs: &GenericPurposeRegs) -> Result<(), CLIError> {
-        if last_syscall_times % 2 == 1 {
-            self.before_call(&regs)?;
-        }
-        if last_syscall_times % 2 == 0 {
-            self.after_call(&regs)?;
-        }
-        Ok(())
-    }
-}
-
-struct AugmentClone {
-    pid: nix::unistd::Pid,
-    ptrace_client: executor::PtraceClient,
-}
-
-impl AugmentSyscall for AugmentClone {
-    fn before_call(&self, regs: &GenericPurposeRegs) -> Result<(), CLIError> {
-        let mut new_regs = regs.clone();
-        let pid2 = self.pid;
-        let new_flag: ptrace::SysNum = libc::CLONE_PTRACE.try_into().or(Err(CLIError::IntoInt))?;
-        new_regs.arg0 |= new_flag;
-        self.ptrace_client
-            .execute(move || ptrace::setregs(pid2, new_regs.clone()))??;
-        let confirm_regs = self
-            .ptrace_client
-            .execute(move || ptrace::getregs(pid2))??;
-        event!(
-            Level::DEBUG,
-            "Clone new arg: {:x}, {:x}, {:x}",
-            confirm_regs.arg0,
-            confirm_regs.arg1,
-            confirm_regs.arg2,
-        );
-        Ok(())
-    }
-
-    fn after_call(&self, regs: &GenericPurposeRegs) -> Result<(), CLIError> {
-        let raw_pid = regs.syscall_retval();
-        if raw_pid > 0 {
-            let child_pid: nix::unistd::Pid =
-                nix::unistd::Pid::from_raw(raw_pid.try_into().or(Err(CLIError::IntoInt))?);
-            event!(Level::INFO, "Clone pid {}", child_pid);
-            let new_ptrace_client = self.ptrace_client.clone();
-            thread::spawn(move || {
-                event_thread(child_pid, new_ptrace_client).unwrap();
-            });
-        }
-        Ok(())
-    }
-}
-
-struct SyscallCounter {
-    syscall: Option<ptrace::SysNum>,
-    times: u64,
-}
-
-impl SyscallCounter {
-    fn count(&mut self, syscall_name: ptrace::SysNum) {
-        let curr_syscall = Some(syscall_name);
-        if self.syscall != curr_syscall {
-            self.syscall = curr_syscall;
-            self.times = 1;
-        } else {
-            self.times += 1;
-        }
-    }
-
-    fn new() -> SyscallCounter {
-        SyscallCounter {
-            syscall: None,
-            times: 0,
-        }
-    }
-}
-
-pub fn event_thread(
-    pid: nix::unistd::Pid,
-    ptrace_client: executor::PtraceClient,
-) -> Result<(), CLIError> {
-    ptrace_client.execute(move || {
-        sys::ptrace::setoptions(pid, sys::ptrace::Options::PTRACE_O_TRACESYSGOOD)
-    })??;
-
-    let augment_clone = AugmentClone {
-        pid,
-        ptrace_client: ptrace_client.clone(),
-    };
-
-    let mut last_syscall = SyscallCounter::new();
-    loop {
-        let span = span!(Level::TRACE, "event_loop", ?pid);
-        let _span_enter = span.enter();
-
-        ptrace_client.execute(move || sys::ptrace::syscall(pid, None))??;
-        let status = ptrace::waitpid_hang(pid)?;
-        event!(Level::TRACE, "child status {:?}", &status);
-
-        if !ptrace::is_trace_stop(&status) && !ptrace::is_still_alive(&status) {
-            break;
-        }
-
-        if !ptrace::is_syscall_stop(&status) {
-            continue;
-        }
-
-        let regs = ptrace_client.execute(move || ptrace::getregs(pid))??;
-        let unknown: String = "Unknown syscall".into();
-        let name = SYSCALL_NAMES.get(&regs.syscall_num).unwrap_or(&unknown);
-
-        last_syscall.count(regs.syscall_num);
-        if last_syscall.times % 2 == 1 {
-            event!(
-                Level::DEBUG,
-                "Syscall {} {} ({:x}, {:x}, {:x})",
-                name,
-                times = &last_syscall.times,
-                arg0 = regs.arg0,
-                arg1 = regs.arg1,
-                arg2 = regs.arg2,
-            );
-        }
-        if regs.syscall_num == libc::SYS_clone {
-            augment_clone.dispatch(last_syscall.times, &regs)?;
-        }
-    }
-    Ok(())
+    #[error("Syscall error: {0}")]
+    SysAugErr(#[from] sysaug::SysAugError),
 }
 
 fn main() -> Result<(), CLIError> {
@@ -177,7 +25,8 @@ fn main() -> Result<(), CLIError> {
     event!(Level::INFO, "First tracee pid: {:?}", pid1);
     thread::spawn(move || {
         let proc1_client2 = proc1_client.clone();
-        let result = event_thread(pid1, proc1_client);
+        let new_tracee_handler = sysaug::TraceeHandler::new(pid1, proc1_client);
+        let result = new_tracee_handler.event_loop();
         proc1_client2.stop();
         result.unwrap();
     });
---
+++ sysaug/src/lib.rs
@@ -0,0 +1,6 @@
+mod clone;
+mod common;
+mod handler;
+
+pub use crate::common::SysAugError;
+pub use crate::handler::TraceeHandler;
