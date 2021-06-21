18913186642ae5819c82b703b0f6388a075e35d0
markzyu <7296488+markzyu@users.noreply.github.com>
2021-06-20 19:24:19 -0700

    Augment paths passed in/out of syscalls, part 1

--- sysaug/src/lib.rs
+++ sysaug/src/lib.rs
@@ -1,6 +1,7 @@
 mod clone;
 mod common;
 mod handler;
+mod paths;
 
 pub use crate::common::SysAugError;
 pub use crate::handler::TraceeHandler;
--- sysaug/src/paths.rs
+++ sysaug/src/paths.rs
@@ -5,23 +5,70 @@ use lazy_static::lazy_static;
 use ptrace::GenericPurposeRegs;
 use std::collections::HashSet;
 use std::convert::TryInto;
+use std::path;
 use std::thread;
 use tracing::{event, Level};
 
 lazy_static! {
     static ref SYSCALL_NAMES: HashSet<ptrace::SysNum> = {
         let mut ans = HashSet::new();
-        ans.insert(libc::SYS_clone);
+        ans.insert(libc::SYS_acct);
+        ans.insert(libc::SYS_chdir);
+        ans.insert(libc::SYS_chroot);
+        ans.insert(libc::SYS_getxattr);
+        ans.insert(libc::SYS_listxattr);
+        ans.insert(libc::SYS_removexattr);
+        ans.insert(libc::SYS_setxattr);
+        ans.insert(libc::SYS_statfs);
+        ans.insert(libc::SYS_swapoff);
+        ans.insert(libc::SYS_swapon);
+        ans.insert(libc::SYS_truncate);
+        ans.insert(libc::SYS_umount2);
+        ans.insert(libc::SYS_lgetxattr);
+        ans.insert(libc::SYS_llistxattr);
+
+        ans.insert(libc::SYS_execve);
+        add_xplat_syscalls(&mut ans);
         ans
     };
 }
 
-pub struct AugmentClone {
-    pub pid: nix::unistd::Pid,
-    pub ptrace_client: executor::PtraceClient,
+#[cfg(target_arch = "arm")]
+fn add_xplat_syscalls(ans: &mut HashSet<ptrace::SysNum>) {
+    ans.insert(libc::SYS_access);
+    ans.insert(libc::SYS_chmod);
+    ans.insert(libc::SYS_chown);
+    ans.insert(libc::SYS_chown32);
+    ans.insert(libc::SYS_mknod);
+    ans.insert(libc::SYS_creat);
+    ans.insert(libc::SYS_stat);
+    ans.insert(libc::SYS_stat64);
+    ans.insert(libc::SYS_statfs64);
+    ans.insert(libc::SYS_truncate64);
+    ans.insert(libc::SYS_uselib);
+    ans.insert(libc::SYS_utimes);
+    ans.insert(libc::SYS_open);
+    ans.insert(libc::SYS_readlink);
+    ans.insert(libc::SYS_lchown);
+    ans.insert(libc::SYS_lchown32);
+    ans.insert(libc::SYS_lstat);
+    ans.insert(libc::SYS_lstat64);
+    ans.insert(libc::SYS_unlink);
+    ans.insert(libc::SYS_rmdir);
+    ans.insert(libc::SYS_mkdir);
 }
 
-impl common::AugmentSyscall for AugmentClone {
+#[cfg(target_arch = "aarch64")]
+fn add_xplat_syscalls(_ans: &mut HashSet<ptrace::SysNum>) {
+}
+
+pub struct AugmentPaths {
+    pub pid: nix::unistd::Pid,
+    pub ptrace_client: executor::PtraceClient,
+    pub chroot: Option<path::PathBuf>,
+}
+
+impl common::AugmentSyscall for AugmentPaths {
     fn valid_calls(&self) -> &HashSet<ptrace::SysNum> {
         &*SYSCALL_NAMES
     }
@@ -64,6 +111,6 @@ impl common::AugmentSyscall for AugmentClone {
     }
 
     fn new(pid: nix::unistd::Pid, ptrace_client: executor::PtraceClient) -> Self {
-        return AugmentClone {pid, ptrace_client}
+        return AugmentPaths {pid, ptrace_client, chroot: None}
     }
 }
