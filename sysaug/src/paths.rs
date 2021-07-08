1700e5c3e66a1613f6aa31495b75d1001c38d3e9
markzyu <7296488+markzyu@users.noreply.github.com>
2021-07-08 12:24:33 -0700

    Augment paths passed in/out of syscalls, part 3
    Now compatible with any syscall that takes paths as input, regardless of
    where the input argument is (0th/1st/2nd register)

--- sysaug/src/paths.rs
+++ sysaug/src/paths.rs
@@ -2,61 +2,84 @@ use crate::common;
 use crate::common::SysAugError;
 use lazy_static::lazy_static;
 use ptrace::GenericPurposeRegs;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::path;
 use tracing::{event, Level};
 
+struct SyscallInfo {
+    /// Bitwise representation
+    path_positions: usize,
+}
+
+macro_rules! define_syscall {
+    ($name:expr, $path_positions:expr, $ans:ident) => {
+        $ans.insert($name as usize, SyscallInfo {path_positions: $path_positions})
+    };
+}
+
 lazy_static! {
+    static ref SYSCALL_INFOS: HashMap<usize, SyscallInfo> = {
+        let mut ans = HashMap::new();
+        define_syscall!(libc::SYS_acct, 1, ans);
+        define_syscall!(libc::SYS_chdir, 1, ans);
+        define_syscall!(libc::SYS_chroot, 1, ans);
+        define_syscall!(libc::SYS_getxattr, 1, ans);
+        define_syscall!(libc::SYS_listxattr, 1, ans);
+        define_syscall!(libc::SYS_removexattr, 1, ans);
+        define_syscall!(libc::SYS_setxattr, 1, ans);
+        define_syscall!(libc::SYS_statfs, 1, ans);
+        define_syscall!(libc::SYS_swapoff, 1, ans);
+        define_syscall!(libc::SYS_swapon, 1, ans);
+        define_syscall!(libc::SYS_truncate, 1, ans);
+        define_syscall!(libc::SYS_umount2, 1, ans);
+        define_syscall!(libc::SYS_lgetxattr, 1, ans);
+        define_syscall!(libc::SYS_llistxattr, 1, ans);
+        define_syscall!(libc::SYS_execve, 1, ans);
+
+        define_syscall!(libc::SYS_openat, 2, ans);
+        define_syscall!(libc::SYS_name_to_handle_at, 2, ans);
+        define_syscall!(libc::SYS_faccessat, 2, ans);
+        define_syscall!(libc::SYS_newfstatat, 2, ans);
+        add_xplat_syscalls(&mut ans);
+        ans
+    };
+
     static ref SYSCALL_NAMES: HashSet<usize> = {
         let mut ans = HashSet::new();
-        ans.insert(libc::SYS_acct as usize);
-        ans.insert(libc::SYS_chdir as usize);
-        ans.insert(libc::SYS_chroot as usize);
-        ans.insert(libc::SYS_getxattr as usize);
-        ans.insert(libc::SYS_listxattr as usize);
-        ans.insert(libc::SYS_removexattr as usize);
-        ans.insert(libc::SYS_setxattr as usize);
-        ans.insert(libc::SYS_statfs as usize);
-        ans.insert(libc::SYS_swapoff as usize);
-        ans.insert(libc::SYS_swapon as usize);
-        ans.insert(libc::SYS_truncate as usize);
-        ans.insert(libc::SYS_umount2 as usize);
-        ans.insert(libc::SYS_lgetxattr as usize);
-        ans.insert(libc::SYS_llistxattr as usize);
-
-        ans.insert(libc::SYS_execve as usize);
-        add_xplat_syscalls(&mut ans);
+        for key in SYSCALL_INFOS.keys() {
+            ans.insert(*key);
+        }
         ans
     };
 }
 
 #[cfg(target_arch = "arm")]
-fn add_xplat_syscalls(ans: &mut HashSet<usize>) {
-    ans.insert(libc::SYS_access as usize);
-    ans.insert(libc::SYS_chmod as usize);
-    ans.insert(libc::SYS_chown as usize);
-    ans.insert(libc::SYS_chown32 as usize);
-    ans.insert(libc::SYS_mknod as usize);
-    ans.insert(libc::SYS_creat as usize);
-    ans.insert(libc::SYS_stat as usize);
-    ans.insert(libc::SYS_stat64 as usize);
-    ans.insert(libc::SYS_statfs64 as usize);
-    ans.insert(libc::SYS_truncate64 as usize);
-    ans.insert(libc::SYS_uselib as usize);
-    ans.insert(libc::SYS_utimes as usize);
-    ans.insert(libc::SYS_open as usize);
-    ans.insert(libc::SYS_readlink as usize);
-    ans.insert(libc::SYS_lchown as usize);
-    ans.insert(libc::SYS_lchown32 as usize);
-    ans.insert(libc::SYS_lstat as usize);
-    ans.insert(libc::SYS_lstat64 as usize);
-    ans.insert(libc::SYS_unlink as usize);
-    ans.insert(libc::SYS_rmdir as usize);
-    ans.insert(libc::SYS_mkdir as usize);
+fn add_xplat_syscalls(ans: &mut HashMap<usize, SyscallInfo>) {
+    define_syscall!(libc::SYS_access, 1, ans);
+    define_syscall!(libc::SYS_chmod, 1, ans);
+    define_syscall!(libc::SYS_chown, 1, ans);
+    define_syscall!(libc::SYS_chown32, 1, ans);
+    define_syscall!(libc::SYS_mknod, 1, ans);
+    define_syscall!(libc::SYS_creat, 1, ans);
+    define_syscall!(libc::SYS_stat, 1, ans);
+    define_syscall!(libc::SYS_stat64, 1, ans);
+    define_syscall!(libc::SYS_statfs64, 1, ans);
+    define_syscall!(libc::SYS_truncate64, 1, ans);
+    define_syscall!(libc::SYS_uselib, 1, ans);
+    define_syscall!(libc::SYS_utimes, 1, ans);
+    define_syscall!(libc::SYS_open, 1, ans);
+    define_syscall!(libc::SYS_readlink, 1, ans);
+    define_syscall!(libc::SYS_lchown, 1, ans);
+    define_syscall!(libc::SYS_lchown32, 1, ans);
+    define_syscall!(libc::SYS_lstat, 1, ans);
+    define_syscall!(libc::SYS_lstat64, 1, ans);
+    define_syscall!(libc::SYS_unlink, 1, ans);
+    define_syscall!(libc::SYS_rmdir, 1, ans);
+    define_syscall!(libc::SYS_mkdir, 1, ans);
 }
 
 #[cfg(target_arch = "aarch64")]
-fn add_xplat_syscalls(_ans: &mut HashSet<usize>) {}
+fn add_xplat_syscalls(_ans: &mut HashMap<usize, SyscallInfo>) {}
 
 pub struct AugmentPaths {
     pub pid: nix::unistd::Pid,
@@ -86,12 +109,21 @@ impl common::AugmentSyscall for AugmentPaths {
 
     fn before_call(&self, regs: &GenericPurposeRegs) -> Result<(), SysAugError> {
         let pid = self.pid;
-        let new_regs = regs.clone();
-        let path = self
-            .ptrace_client
-            .execute(move || ptrace::read_bytes_until_zero(pid, new_regs.arg0))??;
-        let path_str = String::from_utf8_lossy(&path);
-        event!(Level::INFO, "Input Path: {:?}", path_str,);
+        let syscall = SYSCALL_INFOS.get(&regs.syscall_num).unwrap();
+        let possible_args = [regs.arg0, regs.arg1, regs.arg2];
+
+        for (i, ref_arg_i) in possible_args.iter().enumerate() {
+            let check_bit: usize = 1 << i;
+            if (check_bit & syscall.path_positions) == 0 {
+                continue;
+            }
+
+            let arg_i = *ref_arg_i;
+            let path = self
+                .ptrace_client
+                .execute(move || ptrace::read_bytes_until_zero(pid, arg_i))??;
+            event!(Level::INFO, "Input Path: {:?}", String::from_utf8_lossy(&path));
+        }
         Ok(())
     }
 
