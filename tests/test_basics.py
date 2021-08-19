73928893fc28401075faf15fea3b07ea758cf401
markzyu <7296488+markzyu@users.noreply.github.com>
2021-08-19 06:52:24 -0700

    Return exit status of parent command

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -9,3 +9,19 @@ class TestBasics(t.TestCase):
 
     def test_reports_success(self):
         self.assertEqual(c.run("--cmd echo"), 0)
+
+    def test_reports_failure(self):
+        self.assertNotEqual(c.run("--cmd rm"), 0)
+
+    def test_reports_exact_errcode(self):
+        for val in (0, 1, 254, 255):
+            ans = c.run_script(f"exit {val}".encode())
+            self.assertEqual(ans.returncode, val)
+
+        if os.sys.platform.startswith("linux"):
+            # Linux exitcode is 0-255. Exit 256 should return 0
+            ans = c.run_script(b"exit 256")
+            self.assertEqual(ans.returncode, 0)
+            # Linux exitcode is 0-255. Exit 257 should return 1
+            ans = c.run_script(b"exit 257")
+            self.assertEqual(ans.returncode, 1)
