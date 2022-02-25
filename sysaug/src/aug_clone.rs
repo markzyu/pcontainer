35b299a6dd9ae8c3cbbda03514e70dffc5b64bda
markzyu <7296488+markzyu@users.noreply.github.com>
2022-02-25 15:40:26 -0800

    Revert "Catch new child tracees through waitpid instead of PTRACE_EVENT, part 1"
    This reverts commit 1acc75c07cdc5edbcb82c861f8e9187b1ad11edd.

--- sysaug/src/aug_clone.rs
+++ sysaug/src/aug_clone.rs
@@ -6,7 +6,6 @@ use nix::sys;
 use ptrace::GenericPurposeRegs;
 use std::convert::TryInto;
 use std::sync::Arc;
-use tracing::{event, Level};
 
 pub struct AugmentClone<PtraceClient: executor::PtraceClient> {
     pub handler: Arc<TraceeHandler<PtraceClient>>,
@@ -20,24 +19,46 @@ impl<PtraceClient: executor::PtraceClient> common::AugmentSyscall for AugmentClo
     ) -> Result<(), SysAugError> {
         let pid2 = self.handler.pid;
 
-        event!(Level::TRACE, "A");
         let new_flag: usize = libc::CLONE_PTRACE
             .try_into()
             .or(Err(SysAugError::IntoInt))?;
         regs.arg0 |= new_flag;
-        event!(Level::TRACE, "B {:x}", regs.arg0);
         self.handler
             .ptrace_client
             .execute(move || ptrace::setregs(pid2, regs))??;
-        event!(Level::TRACE, "C");
         Ok(())
     }
 
     fn after_call(
         &self,
-        _regs: GenericPurposeRegs,
+        regs: GenericPurposeRegs,
         _syscall: &SyscallInfo,
     ) -> Result<(), SysAugError> {
+        let raw_pid = regs.syscall_retval();
+        if raw_pid > 0 {
+            let child_pid: nix::unistd::Pid =
+                nix::unistd::Pid::from_raw(raw_pid.try_into().or(Err(SysAugError::IntoInt))?);
+
+            self.handler
+                .ptrace_client
+                .prep_attach_to(child_pid, &self.handler.ignore_sigstops)?;
+
+            let new_tracee_handler = self.handler.fork(child_pid)?;
+            let new_tracee_handler2 = Arc::clone(&new_tracee_handler);
+            let root_pid = self.handler.states.root_pid;
+            let fail_fast = self.handler.states.args.fail_fast;
+            new_tracee_handler.start(move || {
+                if fail_fast && new_tracee_handler2.failed() {
+                    let _ = sys::signal::kill(root_pid, Some(sys::signal::Signal::SIGKILL))
+                        .map_err(common::display_err);
+                }
+            });
+
+            self.handler
+                .call_mods(mods::ModFeature::OnCloneComplete, |m| {
+                    m.on_clone_complete(raw_pid as isize)
+                })?;
+        }
         Ok(())
     }
 }
