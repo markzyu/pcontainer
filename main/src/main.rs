c86d8fca1b5787b08a9a1676d5795f870c94958d
mzyu19 <7296488+markzyu@users.noreply.github.com>
2022-05-03 12:47:51 -0700

    wip: show syscalls on Arm64 architecture

--- main/src/main.rs
+++ main/src/main.rs
@@ -3,6 +3,35 @@ use procfs::config_gz;
 use std::thread;
 use std::time::Duration;
 
+macro_rules! seq {
+    // Sequences
+    ($($v:expr,)*) => {
+        std::array::IntoIter::new([$($v,)*]).collect()
+    };
+    ($($v:expr),*) => {
+        std::array::IntoIter::new([$($v,)*]).collect()
+    };
+
+    // Maps
+    ($($k:expr => $v:expr,)*) => {
+        std::array::IntoIter::new([$(($k, $v),)*]).collect()
+    };
+    ($($k:expr => $v:expr),*) => {
+        std::array::IntoIter::new([$(($k, $v),)*]).collect()
+    };
+}
+
+use std::collections::HashMap;
+
+fn syscall_names() -> HashMap<libc::c_long, String> { 
+    let mut map = HashMap::new();
+    map.insert(libc::SYS_openat, "openat".into());
+    map.insert(libc::SYS_close, "close".into());
+    map.insert(libc::SYS_read, "read".into());
+    map.insert(libc::SYS_write, "write".into());
+    map
+}
+
 fn main() {
     let mut config = config_gz::ConfigGz::default();
     config.init_from_host_os().unwrap();
@@ -18,12 +47,32 @@ fn main() {
         }
     }
 
+    let overrides = ptrace::SysOverrideList::default();
+    let names = syscall_names();
+
     let mut cmd = std::process::Command::new("ls");
     let child = ptrace::start(&mut cmd).unwrap();
     let pid = ptrace::pid(&child).unwrap();
-    sys::ptrace::setoptions(pid, sys::ptrace::Options::PTRACE_O_TRACEEXIT).unwrap();
-    sys::ptrace::cont(pid, None).unwrap();
-    thread::sleep(Duration::from_millis(20)); // Note: without sleep, wait will return StillAlive instead.
-    let status = ptrace::wait(&child).unwrap();
-    dbg!(status);
+    dbg!(pid);
+    loop {
+				dbg!("before syscall");
+        sys::ptrace::syscall(pid, None).unwrap();
+				dbg!("after syscall");
+        let status = ptrace::wait_hang(&child).unwrap();
+				dbg!(status);
+				let regs = ptrace::getregset1(pid).unwrap();
+        let unknown: String = "Unknown syscall".into();
+				let name = names.get(&regs.syscall_num).unwrap_or(&unknown);
+        dbg!(name);
+        /*
+        if ptrace::is_trace_stop(&status) {
+            let regs = ptrace::getregset1(pid).unwrap();
+            dbg!(regs);
+            //overrides.on_syscall(&mut regs);
+        } else if !ptrace::is_still_alive(&status) {
+            break
+        }
+        */
+    }
+    thread::sleep(Duration::from_millis(10));
 }
