27035ee31faefd6ef4f7c2182dbe0186734f70aa
mzyu19 <7296488+markzyu@users.noreply.github.com>
2022-05-03 12:47:55 -0700

    Add cli option to turn on/off strace-style printing of syscalls

--- main/src/main.rs
+++ main/src/main.rs
@@ -16,17 +16,26 @@ pub enum CLIError {
 }
 
 fn main() -> Result<(), CLIError> {
+    // Initialize, parse args
     tracing_subscriber::fmt::init();
-    let (proc1_client, ptrace_loop) = executor::new_ptrace_executor();
+    let strace = std::env::args().any(|arg| arg == "--strace");
 
-    let mut cmd = std::process::Command::new("bash");
-    let child = ptrace::start(&mut cmd)?;
-
-    let mods: Vec<ModProvider> = vec![mods::ChrootMod::new_box, mods::TraceChildMod::new_box];
-
-    let pid1 = ptrace::pid(&child)?;
+    // Spawn first tracee
+    let pid1 = {
+        let mut cmd = std::process::Command::new("bash");
+        let child = ptrace::start(&mut cmd)?;
+        ptrace::pid(&child)?
+    };
     event!(Level::INFO, "First tracee pid: {:?}", pid1);
 
+    // Setup mods
+    let mut mods: Vec<ModProvider> = vec![mods::ChrootMod::new_box, mods::TraceChildMod::new_box];
+    if strace {
+        mods.push(mods::StraceMod::new_box);
+    }
+
+    // Start tracee handler thread
+    let (proc1_client, ptrace_loop) = executor::new_ptrace_executor();
     let new_tracee_handler = sysaug::TraceeHandler::new(pid1, proc1_client.clone(), mods)?;
     thread::spawn(move || {
         let proc1_client2 = proc1_client.clone();
--- mods/src/strace.rs
+++ mods/src/strace.rs
@@ -3,33 +3,44 @@ use lazy_static::lazy_static;
 use std::collections::HashSet;
 use std::sync::Arc;
 use sysaug::mods::{Mod, ModFeature};
-use sysaug::TraceeHandler;
+use sysaug::{SysAugError, TraceeHandler};
+use tracing::{event, Level};
 
 lazy_static! {
     static ref DEFAULT_LISTENER_SPEC: HashSet<ModFeature> = {
-        let ans = HashSet::new();
+        let mut ans = HashSet::new();
+        ans.insert(ModFeature::OnFilePath);
         ans
     };
 }
 
-pub struct ChrootMod {
+pub struct StraceMod {
     handler: Arc<TraceeHandler>,
 }
 
-impl ChrootMod {
+impl StraceMod {
     pub fn new_box(handler: Arc<TraceeHandler>) -> Box<dyn Mod> {
-        Box::new(ChrootMod { handler })
+        Box::new(StraceMod { handler })
     }
 }
 
-impl Mod for ChrootMod {
+impl Mod for StraceMod {
     fn clone_box(&self) -> Box<dyn Mod + Send + Sync> {
-        Box::new(ChrootMod {
+        Box::new(StraceMod {
             handler: Arc::clone(&self.handler),
         })
     }
 
     fn get_features(&self) -> &'static HashSet<ModFeature> {
         &*DEFAULT_LISTENER_SPEC
+    }
+
+    fn on_file_path(&self, raw_path: &[u8]) -> Result<(), SysAugError> {
+        event!(
+            Level::INFO,
+            "Input Path: {:?}",
+            String::from_utf8_lossy(raw_path)
+        );
+        Ok(())
     }
 }
