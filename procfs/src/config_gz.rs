a823756851a2b1d7388cb9c78017cdf8e3db8968
mzyu19 <7296488+markzyu@users.noreply.github.com>
2022-05-03 12:47:51 -0700

    lint

--- procfs/src/config_gz.rs
+++ procfs/src/config_gz.rs
@@ -1,77 +1,76 @@
+use flate2::bufread::GzDecoder;
 use std::error;
 use std::io;
 use std::io::BufRead;
-use flate2::bufread::GzDecoder;
 
 #[derive(Debug)]
 pub struct ConfigGzLine {
-  content: Vec<u8>
+    content: Vec<u8>,
 }
 
 #[derive(Debug)]
 pub struct ConfigGz {
-  pub lines: Vec<ConfigGzLine>
+    pub lines: Vec<ConfigGzLine>,
 }
 
 const WHITESPACES: &[u8] = b" \t\n\r";
 
 fn _strip_slice(input: &[u8]) -> &[u8] {
-  if let Some(left) = input.iter().position(|x| !WHITESPACES.contains(x)) {
-    let right = input.iter().rposition(|x| !WHITESPACES.contains(x)).unwrap();
-    return &input[left..right+1]
-  }
-  return &input[0..0]
+    if let Some(left) = input.iter().position(|x| !WHITESPACES.contains(x)) {
+        let right = input
+            .iter()
+            .rposition(|x| !WHITESPACES.contains(x))
+            .unwrap();
+        return &input[left..right + 1];
+    }
+    &input[0..0]
 }
 
 impl ConfigGzLine {
-  fn preproc(&self) -> Option<(&[u8], usize)> {
-    let mut content = self.content.as_slice();
-    if let Some(ignore_idx) = content.iter().position(|x| x == &b'#') {
-      content = &(content[..ignore_idx]);
+    fn preproc(&self) -> Option<(&[u8], usize)> {
+        let mut content = self.content.as_slice();
+        if let Some(ignore_idx) = content.iter().position(|x| x == &b'#') {
+            content = &(content[..ignore_idx]);
+        }
+        let eq_idx = content.iter().position(|x| x == &b'=')?;
+        Some((content, eq_idx))
     }
-    let eq_idx = content.iter().position(|x| x == &b'=')?;
-    return Some((content, eq_idx));
-  }
 
-  pub fn maybe_value(&self) -> Option<(&[u8], usize)> {
-    let (content, idx) = self.preproc()?;
-    return Some((
-      _strip_slice(content.get(idx+1..).unwrap_or(b"")),
-      idx
-    ));
-  }
+    pub fn maybe_value(&self) -> Option<(&[u8], usize)> {
+        let (content, idx) = self.preproc()?;
+        Some((_strip_slice(content.get(idx + 1..).unwrap_or(b"")), idx))
+    }
 
-  pub fn maybe_name(&self) -> Option<(&[u8], usize)> {
-    let (content, idx) = self.preproc()?;
-    return Some((
-      _strip_slice(content.get(..idx).unwrap_or(b"")),
-      idx
-    ));
-  }
+    pub fn maybe_name(&self) -> Option<(&[u8], usize)> {
+        let (content, idx) = self.preproc()?;
+        Some((_strip_slice(content.get(..idx).unwrap_or(b"")), idx))
+    }
 }
 
 impl ConfigGz {
-  pub fn init_from_host_os(&mut self) -> Result<(), Box<dyn error::Error>> {
-    let f = io::BufReader::new(std::fs::File::open("/proc/config.gz")?);
-    let mut gunzip = io::BufReader::new(GzDecoder::new(f));
+    pub fn init_from_host_os(&mut self) -> Result<(), Box<dyn error::Error>> {
+        let f = io::BufReader::new(std::fs::File::open("/proc/config.gz")?);
+        let mut gunzip = io::BufReader::new(GzDecoder::new(f));
 
-    let mut line_buf = vec![];
-    self.lines.clear();
-    loop {
-      line_buf.clear();
-      let nbytes_read = gunzip.read_until(b'\n', &mut line_buf)?;
-      if nbytes_read == 0 {
-        break;
-      }
+        let mut line_buf = vec![];
+        self.lines.clear();
+        loop {
+            line_buf.clear();
+            let nbytes_read = gunzip.read_until(b'\n', &mut line_buf)?;
+            if nbytes_read == 0 {
+                break;
+            }
 
-      self.lines.push(ConfigGzLine {content: line_buf.clone()});
+            self.lines.push(ConfigGzLine {
+                content: line_buf.clone(),
+            });
+        }
+        Ok(())
     }
-    Ok(())
-  }
 
-  pub fn lines(&self) -> &[ConfigGzLine] {
-    self.lines.as_slice()
-  }
+    pub fn lines(&self) -> &[ConfigGzLine] {
+        self.lines.as_slice()
+    }
 }
 
 #[cfg(test)]
@@ -81,32 +80,102 @@ mod tests {
     #[test]
     fn config_gz_line_maybe_value() {
         let test_cases: Vec<(config_gz::ConfigGzLine, Option<(&[u8], usize)>)> = vec![
-          (config_gz::ConfigGzLine{content: b"".to_vec()}, None),
-          (config_gz::ConfigGzLine{content: b"#comment a=123".to_vec()}, None),
-          (config_gz::ConfigGzLine{content: b"INVALID_VAR".to_vec()}, None),
-          (config_gz::ConfigGzLine{content: b"CONFIG_ABC_DEF=ab".to_vec()}, Some((b"ab", 14))),
-          (config_gz::ConfigGzLine{content: b"CONFIG_ABC_DEFG=cd \t ".to_vec()}, Some((b"cd", 15))),
-          (config_gz::ConfigGzLine{content: b"CONFIG_ABC_DEF=xy \t # def".to_vec()}, Some((b"xy", 14))),
-          (config_gz::ConfigGzLine{content: b"CONFIG_ABC_DEF= xy \t # def".to_vec()}, Some((b"xy", 14))),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"".to_vec(),
+                },
+                None,
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"#comment a=123".to_vec(),
+                },
+                None,
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"INVALID_VAR".to_vec(),
+                },
+                None,
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"CONFIG_ABC_DEF=ab".to_vec(),
+                },
+                Some((b"ab", 14)),
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"CONFIG_ABC_DEFG=cd \t ".to_vec(),
+                },
+                Some((b"cd", 15)),
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"CONFIG_ABC_DEF=xy \t # def".to_vec(),
+                },
+                Some((b"xy", 14)),
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"CONFIG_ABC_DEF= xy \t # def".to_vec(),
+                },
+                Some((b"xy", 14)),
+            ),
         ];
         for (line, expect) in test_cases {
-          assert_eq!(line.maybe_value(), expect);
+            assert_eq!(line.maybe_value(), expect);
         }
     }
 
     #[test]
     fn config_gz_line_maybe_name() {
         let test_cases: Vec<(config_gz::ConfigGzLine, Option<(&[u8], usize)>)> = vec![
-          (config_gz::ConfigGzLine{content: b"".to_vec()}, None),
-          (config_gz::ConfigGzLine{content: b"#comment a=123".to_vec()}, None),
-          (config_gz::ConfigGzLine{content: b"INVALID_VAR".to_vec()}, None),
-          (config_gz::ConfigGzLine{content: b"CONFIG_ABC_DEF=ab".to_vec()}, Some((b"CONFIG_ABC_DEF", 14))),
-          (config_gz::ConfigGzLine{content: b"CONFIG_ABC_DEFG=cd \t ".to_vec()}, Some((b"CONFIG_ABC_DEFG", 15))),
-          (config_gz::ConfigGzLine{content: b" CONFIG_ABC_DEF=xy \t # def".to_vec()}, Some((b"CONFIG_ABC_DEF", 15))),
-          (config_gz::ConfigGzLine{content: b"CONFIG_ABC_DEF = xy \t # def".to_vec()}, Some((b"CONFIG_ABC_DEF", 15))),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"".to_vec(),
+                },
+                None,
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"#comment a=123".to_vec(),
+                },
+                None,
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"INVALID_VAR".to_vec(),
+                },
+                None,
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"CONFIG_ABC_DEF=ab".to_vec(),
+                },
+                Some((b"CONFIG_ABC_DEF", 14)),
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"CONFIG_ABC_DEFG=cd \t ".to_vec(),
+                },
+                Some((b"CONFIG_ABC_DEFG", 15)),
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b" CONFIG_ABC_DEF=xy \t # def".to_vec(),
+                },
+                Some((b"CONFIG_ABC_DEF", 15)),
+            ),
+            (
+                config_gz::ConfigGzLine {
+                    content: b"CONFIG_ABC_DEF = xy \t # def".to_vec(),
+                },
+                Some((b"CONFIG_ABC_DEF", 15)),
+            ),
         ];
         for (line, expect) in test_cases {
-          assert_eq!(line.maybe_name(), expect);
+            assert_eq!(line.maybe_name(), expect);
         }
     }
 }
--- ptrace/src/lib.rs
+++ ptrace/src/lib.rs
@@ -8,42 +8,45 @@ type AnyResult<V> = Result<V, Box<dyn std::error::Error>>;
 
 #[derive(Debug, Clone)]
 pub struct PTraceError<'a> {
-  reason: &'a str
+    reason: &'a str,
 }
 impl<'a> fmt::Display for PTraceError<'a> {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "PTrace Error: {}", self.reason)
-  }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PTrace Error: {}", self.reason)
+    }
 }
 
 pub fn is_trace_stop(status: &wait::WaitStatus) -> bool {
-  matches!(status, wait::WaitStatus::PtraceEvent(_, _, _))
+    matches!(status, wait::WaitStatus::PtraceEvent(_, _, _))
 }
 
 pub fn is_still_alive(status: &wait::WaitStatus) -> bool {
-  matches!(status, wait::WaitStatus::StillAlive)
+    matches!(status, wait::WaitStatus::StillAlive)
 }
 
 pub fn start(cmd: &mut process::Command) -> AnyResult<process::Child> {
-  Ok(cmd.spawn_ptrace()?)
+    Ok(cmd.spawn_ptrace()?)
 }
 
 pub fn pid(child: &process::Child) -> AnyResult<nix::unistd::Pid> {
-  let pid: i32 = child.id().try_into()?;
-  Ok(nix::unistd::Pid::from_raw(pid))
+    let pid: i32 = child.id().try_into()?;
+    Ok(nix::unistd::Pid::from_raw(pid))
 }
 
 pub fn wait(child: &process::Child) -> AnyResult<wait::WaitStatus> {
-  Ok(wait::waitpid(pid(&child)?, Some(wait::WaitPidFlag::WNOHANG))?)
+    Ok(wait::waitpid(
+        pid(&child)?,
+        Some(wait::WaitPidFlag::WNOHANG),
+    )?)
 }
 
 #[cfg(test)]
 mod tests {
-    use nix::sys::wait;
     use nix::sys::ptrace;
+    use nix::sys::wait;
     use ntest::timeout;
-    use std::time::Duration;
     use std::thread;
+    use std::time::Duration;
 
     fn _start_cmd() -> std::process::Child {
         let mut cmd = std::process::Command::new("ls");
@@ -68,7 +71,7 @@ mod tests {
         ptrace::setoptions(pid.clone(), ptrace::Options::PTRACE_O_TRACEEXIT).unwrap();
         ptrace::cont(pid.clone(), None).unwrap();
 
-        thread::sleep(Duration::from_millis(20));  // Note: without sleep, wait will return StillAlive instead.
+        thread::sleep(Duration::from_millis(20)); // Note: without sleep, wait will return StillAlive instead.
         let status = crate::wait(&child).unwrap();
         assert!(matches!(status, wait::WaitStatus::PtraceEvent(_, _, _)));
         assert!(crate::is_trace_stop(&status));
