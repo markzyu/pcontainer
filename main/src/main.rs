1bb0672aa9512264177c5830594c0f857795ecd7
mzyu19 <7296488+markzyu@users.noreply.github.com>
2022-05-03 12:47:53 -0700

    Modularize clone() logic

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,5 +1,6 @@
 use lazy_static::lazy_static;
 use nix::sys;
+use ptrace::GenericPurposeRegs;
 use std::collections::HashMap;
 use std::convert::TryInto;
 use std::thread;
@@ -33,6 +34,85 @@ pub enum CLIError {
     IntoInt,
 }
 
+trait AugmentSyscall {
+    fn before_call(&self, regs: &GenericPurposeRegs) -> Result<(), CLIError>;
+    fn after_call(&self, regs: &GenericPurposeRegs) -> Result<(), CLIError>;
+    fn dispatch(&self, last_syscall_times: u64, regs: &GenericPurposeRegs) -> Result<(), CLIError> {
+        if last_syscall_times % 2 == 1 {
+            self.before_call(&regs)?;
+        }
+        if last_syscall_times % 2 == 0 {
+            self.after_call(&regs)?;
+        }
+        Ok(())
+    }
+}
+
+struct AugmentClone {
+    pid: nix::unistd::Pid,
+    ptrace_client: executor::PtraceClient,
+}
+
+impl AugmentSyscall for AugmentClone {
+    fn before_call(&self, regs: &GenericPurposeRegs) -> Result<(), CLIError> {
+        let mut new_regs = regs.clone();
+        let pid2 = self.pid;
+        let new_flag: ptrace::SysNum = libc::CLONE_PTRACE.try_into().or(Err(CLIError::IntoInt))?;
+        new_regs.arg0 |= new_flag;
+        self.ptrace_client
+            .execute(move || ptrace::setregs(pid2, new_regs.clone()))??;
+        let confirm_regs = self
+            .ptrace_client
+            .execute(move || ptrace::getregs(pid2))??;
+        event!(
+            Level::DEBUG,
+            "Clone new arg: {:x}, {:x}, {:x}",
+            confirm_regs.arg0,
+            confirm_regs.arg1,
+            confirm_regs.arg2,
+        );
+        Ok(())
+    }
+
+    fn after_call(&self, regs: &GenericPurposeRegs) -> Result<(), CLIError> {
+        let raw_pid = regs.syscall_retval();
+        if raw_pid > 0 {
+            let child_pid: nix::unistd::Pid =
+                nix::unistd::Pid::from_raw(raw_pid.try_into().or(Err(CLIError::IntoInt))?);
+            event!(Level::INFO, "Clone pid {}", child_pid);
+            let new_ptrace_client = self.ptrace_client.clone();
+            thread::spawn(move || {
+                event_thread(child_pid, new_ptrace_client).unwrap();
+            });
+        }
+        Ok(())
+    }
+}
+
+struct SyscallCounter {
+    name: Option<String>,
+    times: u64,
+}
+
+impl SyscallCounter {
+    fn count(&mut self, syscall_name: &str) {
+        let curr_syscall = Some(syscall_name.to_string());
+        if self.name != curr_syscall {
+            self.name = curr_syscall;
+            self.times = 1;
+        } else {
+            self.times += 1;
+        }
+    }
+
+    fn new() -> SyscallCounter {
+        SyscallCounter {
+            name: None,
+            times: 0,
+        }
+    }
+}
+
 pub fn event_thread(
     pid: nix::unistd::Pid,
     ptrace_client: executor::PtraceClient,
@@ -41,8 +121,12 @@ pub fn event_thread(
         sys::ptrace::setoptions(pid, sys::ptrace::Options::PTRACE_O_TRACESYSGOOD)
     })??;
 
-    let mut last_syscall: Option<String> = None;
-    let mut last_syscall_times: u64 = 0;
+    let augment_clone = AugmentClone {
+        pid,
+        ptrace_client: ptrace_client.clone(),
+    };
+
+    let mut last_syscall = SyscallCounter::new();
     loop {
         let span = span!(Level::TRACE, "event_loop", ?pid);
         let _span_enter = span.enter();
@@ -63,53 +147,20 @@ pub fn event_thread(
         let unknown: String = "Unknown syscall".into();
         let name = SYSCALL_NAMES.get(&regs.syscall_num).unwrap_or(&unknown);
 
-        let curr_syscall = Some(name.clone());
-        if last_syscall != curr_syscall {
-            last_syscall = curr_syscall;
-            last_syscall_times = 1;
-        } else {
-            last_syscall_times += 1;
-        }
-
-        if last_syscall_times % 2 == 1 {
+        last_syscall.count(&name);
+        if last_syscall.times % 2 == 1 {
             event!(
                 Level::DEBUG,
                 "Syscall {} {} ({:x}, {:x}, {:x})",
                 name,
-                times = &last_syscall_times,
+                times = &last_syscall.times,
                 arg0 = regs.arg0,
                 arg1 = regs.arg1,
                 arg2 = regs.arg2,
             );
         }
-        // Before clone()
-        if name == "clone" && last_syscall_times % 2 == 1 {
-            let mut new_regs = regs.clone();
-            let new_flag: ptrace::SysNum =
-                libc::CLONE_PTRACE.try_into().or(Err(CLIError::IntoInt))?;
-            new_regs.arg0 |= new_flag;
-            ptrace_client.execute(move || ptrace::setregs(pid, new_regs.clone()))??;
-            let confirm_regs = ptrace_client.execute(move || ptrace::getregs(pid))??;
-            event!(
-                Level::DEBUG,
-                "Clone new arg: {:x}, {:x}, {:x}",
-                confirm_regs.arg0,
-                confirm_regs.arg1,
-                confirm_regs.arg2,
-            );
-        }
-        // After clone()
-        if name == "clone" && last_syscall_times % 2 == 0 {
-            let raw_pid = regs.syscall_retval();
-            if raw_pid > 0 {
-                let child_pid: nix::unistd::Pid =
-                    nix::unistd::Pid::from_raw(raw_pid.try_into().or(Err(CLIError::IntoInt))?);
-                event!(Level::INFO, "Clone pid {}", child_pid);
-                let new_ptrace_client = ptrace_client.clone();
-                thread::spawn(move || {
-                    event_thread(child_pid, new_ptrace_client).unwrap();
-                });
-            }
+        if name == "clone" {
+            augment_clone.dispatch(last_syscall.times, &regs)?;
         }
     }
     Ok(())
