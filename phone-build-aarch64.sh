38187ecbe54aeb4c4253b3efd7d30422592f1979
markzyu <7296488+markzyu@users.noreply.github.com>
2026-09-03 19:17:29 +0100

    chore: remove old outdated scripts

--- phone-build-aarch64.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TARGET=${1:-dc177}
-cargo build --target=aarch64-unknown-linux-musl --release
-scp target/aarch64-unknown-linux-musl/release/dockify "$TARGET":
--- phone-kill.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TARGET=${1:-dc156}
-pid="$(ssh "$TARET" 'ps' | grep dockify | awk '{print $2}')"
-echo Pid: $pid
-ssh "$TARET" "kill $pid"
-ssh "$TARET" "cat log" | grep -m1 -C50 TTIN
